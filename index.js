0a09daf1db7d8b7a21388f7ba2a3bfa9a62d456e
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-10-24 14:03:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // add solution here
-var musicians = ['John Lennon','Paul McCartney', 'George Harrison', 'Ringo Starr']
+var musicians = ['John Lennon','Paul McCartney', 'George Harrison', 'Ringo Starr'];
+var instruments = ['guitar', ]

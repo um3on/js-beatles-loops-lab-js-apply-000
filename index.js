5543a6bb95cb41a8f20c084528842438118d0324
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-10-24 14:06:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ var musicians = ['John Lennon','Stuart Sutcliffe', 'Tommy Moore', 'Ringo Starr']
 var instruments = ['guitar', 'bass', 'drum kits', 'drum kits'];
 var myArray = [];
 
-for(var i =0; i<=musicians.length)
+//Just for this specific example, i didnt check the lebgth of instruments and musicians to be equal
+for(var i =0; i < musicians.length; i++) {
+
+}

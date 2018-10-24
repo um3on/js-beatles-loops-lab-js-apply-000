683fbf8d4a9fd855e7964ab3818e01de9dd078d5
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-10-24 14:19:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ function theBeatlesPlay(musc,inst) {
 
   return myArray;
 }
+
+const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];

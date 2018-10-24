5e720b75373091b8b44084c04807989322b93c40
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-10-24 14:05:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 var musicians = ['John Lennon','Stuart Sutcliffe', 'Tommy Moore', 'Ringo Starr'];
 var instruments = ['guitar', 'bass', 'drum kits', 'drum kits'];
 var myArray = [];
+
+for(var i =0; i<=musicians.length)

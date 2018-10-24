5fcea6a946e67a10d276bf7672947d65c3d4071d
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-10-24 14:10:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // add solution here
 var musicians = ['John Lennon','Stuart Sutcliffe', 'Tommy Moore', 'Ringo Starr'];
 var instruments = ['guitar', 'bass', 'drum kits', 'drum kits'];
-var myArray = [];
 
-theBeatlesPlay(musicians,instruments)
-fucntion theBeatlesPlay(musc,inst)
-//Just for this specific example, i didnt check the lebgth of instruments and musicians to be equal
-for(var i =0; i < musicians.length; i++) {
-  myArray[i] = musicians[i] + " plays " + instruments[i];
+
+theBeatlesPlay(musicians,instruments);
+fucntion theBeatlesPlay(musc,inst) {
+  //Just for this specific example, i didnt check the lebgth of instruments and musicians to be equal
+  for(var i =0; i < musc.length; i++) {
+    myArray[i] = musicians[i] + " plays " + instruments[i];
+  }
 }

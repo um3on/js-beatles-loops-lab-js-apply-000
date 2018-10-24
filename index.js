166623c818a522e71695ab8eef80a2824e9c45fc
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-10-24 14:10:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var instruments = ['guitar', 'bass', 'drum kits', 'drum kits'];
 
 theBeatlesPlay(musicians,instruments);
 fucntion theBeatlesPlay(musc,inst) {
+  var myArray = [];
+  
   //Just for this specific example, i didnt check the lebgth of instruments and musicians to be equal
   for(var i =0; i < musc.length; i++) {
     myArray[i] = musc[i] + " plays " + inst[i];
